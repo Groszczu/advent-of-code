abd7888546afc5784278423013a64d2c26c217e6
Roch Goszczyński <roch.goszczynski@gmail.com>
2022-12-06 13:22:50 +0100

    refactor: add crane struct

--- src/day5.rs
+++ src/day5.rs
@@ -1,85 +1,59 @@
+use std::str::FromStr;
+
 use crate::{shared::PuzzleResult, test_puzzle};
 
 mod crane;
 
+use crane::{Crane, CrateMover, MoveInstruction};
+
 pub fn part1(input: &str) -> PuzzleResult {
-    let (mut stacks, instructions) = transform_input(input);
+    let (mut crane, move_instructions) = transform_input(input);
 
-    for instruction in instructions {
-        let (quantity, from, to) = instruction;
+    println!("{:?}", crane);
 
-        let crates_to_move = pick_crates_up(&mut stacks, quantity, from);
-
-        put_creates_down(&mut stacks, to, crates_to_move.iter().copied().rev());
+    for move_instruction in move_instructions {
+        crane.move_containers(&move_instruction, CrateMover::V9000);
     }
 
-    get_result(&stacks)
+    get_result(&crane)
 }
 
 pub fn part2(input: &str) -> PuzzleResult {
-    let (mut stacks, instructions) = transform_input(input);
+    let (mut crane, move_instructions) = transform_input(input);
 
-    for instruction in instructions {
-        let (quantity, from, to) = instruction;
-
-        let crates_to_move = pick_crates_up(&mut stacks, quantity, from);
-
-        put_creates_down(&mut stacks, to, crates_to_move.iter().copied());
+    for move_instruction in move_instructions {
+        crane.move_containers(&move_instruction, CrateMover::V9001);
     }
 
-    get_result(&stacks)
+    get_result(&crane)
 }
 
-fn transform_input(input: &str) -> (Vec<Vec<&str>>, Vec<(i32, i32, i32)>) {
+fn transform_input(input: &str) -> (Crane, Vec<MoveInstruction>) {
     let input_parts: Vec<_> = input.split("\n\n").collect();
     let start_position = input_parts[0];
     let move_instructions = input_parts[1];
 
-    let start_position: Vec<_> = start_position
-        .lines()
-        .map(|crates| crates.split(' ').collect::<Vec<&str>>())
-        .collect();
+    let crane = Crane::from_str(start_position).expect("start position should be valid");
 
     let move_instructions: Vec<_> = move_instructions
         .lines()
         .map(|instruction| {
-            let instruction_parts = instruction
+            instruction
                 .split(' ')
-                .filter_map(|part| part.parse::<i32>().ok())
-                .collect::<Vec<i32>>();
-
-            (
-                instruction_parts[0],
-                instruction_parts[1],
-                instruction_parts[2],
-            )
+                .filter_map(|part| part.parse::<usize>().ok())
+                .collect::<MoveInstruction>()
         })
         .collect();
 
-    (start_position, move_instructions)
+    (crane, move_instructions)
 }
 
-fn pick_crates_up<'a>(stacks: &mut Vec<Vec<&'a str>>, quantity: i32, from: i32) -> Vec<&'a str> {
-    let from_index = (from - 1) as usize;
-    let from_len = stacks[from_index].len();
-    stacks[from_index].split_off(from_len - quantity as usize)
-}
-
-fn put_creates_down<'a>(
-    stacks: &mut Vec<Vec<&'a str>>,
-    to: i32,
-    crates: impl Iterator<Item = &'a str>,
-) -> () {
-    let to_index = (to - 1) as usize;
-    stacks[to_index].extend(crates);
-}
-
-fn get_result(stacks: &Vec<Vec<&str>>) -> PuzzleResult {
-    stacks
-        .iter()
-        .map(|stack| *stack.last().expect("crate stack should have an element"))
-        .collect::<Vec<_>>()
-        .join("")
+fn get_result(crane: &Crane) -> PuzzleResult {
+    crane
+        .top_containers()
+        .map(|container| *container.expect("all top containers should exist"))
+        .map(|container| container.label())
+        .collect::<String>()
         .into()
 }
 
---
+++ src/day5/crane.rs
@@ -0,0 +1,149 @@
+use std::str::FromStr;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Container {
+    label: char,
+}
+
+impl Container {
+    pub fn label(&self) -> char {
+        self.label
+    }
+}
+
+impl From<char> for Container {
+    fn from(label: char) -> Self {
+        Self { label }
+    }
+}
+
+#[derive(Debug)]
+pub struct MoveInstruction {
+    pub quantity: usize,
+    pub from: usize,
+    pub to: usize,
+}
+
+impl FromIterator<usize> for MoveInstruction {
+    fn from_iter<T: IntoIterator<Item = usize>>(iter: T) -> Self {
+        let mut iter = iter.into_iter();
+        let quantity = iter.next().expect("iter should have at least 1 element");
+        let from = iter.next().expect("iter should have at least 2 elements");
+        let to = iter.next().expect("iter should have at least 3 elements");
+
+        Self { quantity, from, to }
+    }
+}
+
+pub enum CrateMover {
+    V9000,
+    V9001,
+}
+
+impl CrateMover {
+    pub fn order_containers(&self, containers_to_move: Vec<Container>) -> Vec<Container> {
+        match self {
+            Self::V9000 => containers_to_move.iter().cloned().rev().collect::<Vec<_>>(),
+            Self::V9001 => containers_to_move,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Crane {
+    containers_stacks: Vec<Vec<Container>>,
+}
+
+impl Crane {
+    fn pick_containers_up<'a>(&mut self, move_instruction: &MoveInstruction) -> Vec<Container> {
+        let from_index = move_instruction.from - 1;
+        let from_len = self.containers_stacks[from_index].len();
+
+        self.containers_stacks[from_index].split_off(from_len - move_instruction.quantity)
+    }
+
+    fn put_containers_down(
+        &mut self,
+        move_instruction: &MoveInstruction,
+        containers_iter: impl IntoIterator<Item = Container>,
+    ) -> () {
+        let to_index = move_instruction.to - 1;
+        self.containers_stacks[to_index].extend(containers_iter);
+    }
+
+    pub fn move_containers(
+        &mut self,
+        move_instruction: &MoveInstruction,
+        crate_mover: CrateMover,
+    ) -> () {
+        let containers_to_move = self.pick_containers_up(move_instruction);
+
+        let ordered_containers = crate_mover.order_containers(containers_to_move);
+        self.put_containers_down(move_instruction, ordered_containers);
+    }
+
+    pub fn top_containers(&self) -> impl Iterator<Item = Option<&Container>> {
+        self.containers_stacks.iter().map(|stack| stack.last())
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum ParseCraneError {
+    ExpectedSeparatorError,
+    ExpectedNextTokenToExistError,
+    UnexpectedTokenError(char),
+}
+
+impl FromStr for Crane {
+    type Err = ParseCraneError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let tokens = s.chars().collect::<Vec<_>>();
+
+        let mut current_token_index = 0usize;
+        let mut stack_index = 0usize;
+        let mut expect_separator = false;
+        let mut containers_stacks = Vec::<Vec<Container>>::new();
+
+        while let Some(&token) = tokens.get(current_token_index) {
+            if containers_stacks.get(stack_index).is_none() {
+                containers_stacks.push(Vec::new());
+            }
+            if expect_separator {
+                if token == ' ' {
+                    stack_index += 1;
+                } else if token == '\n' {
+                    stack_index = 0;
+                } else {
+                    return Err(ParseCraneError::ExpectedSeparatorError);
+                }
+                current_token_index += 1;
+                expect_separator = false;
+                continue;
+            }
+            if token == '[' {
+                let label = tokens
+                    .get(current_token_index + 1)
+                    .ok_or(ParseCraneError::ExpectedNextTokenToExistError)?;
+                let container = Container::from(*label);
+                containers_stacks[stack_index].insert(0, container);
+                current_token_index += 3;
+                expect_separator = true;
+            } else if token == ' ' {
+                if tokens
+                    .get(current_token_index + 1)
+                    .ok_or(ParseCraneError::ExpectedNextTokenToExistError)?
+                    .is_numeric()
+                {
+                    break;
+                }
+                current_token_index += 3;
+                expect_separator = true;
+            } else {
+                return Err(ParseCraneError::UnexpectedTokenError(token));
+            }
+        }
+
+        Ok(Crane { containers_stacks })
+    }
+}
